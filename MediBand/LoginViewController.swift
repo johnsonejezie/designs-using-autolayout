9b7946cea9b9a1d3d8517cd858174e9dd73d0f1d
Johnson Ejezie <johnsonejezie@Johnsons-MacBook-Pro.local>
2015-07-14 21:44:15 +0100

    added sign up view

---
+++ MediBand/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  MediBand
+//
+//  Created by Johnson Ejezie on 7/14/15.
+//  Copyright (c) 2015 Johnson Ejezie. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var userNameTextfield: UITextField!
+    
+
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameTextfield.layer.cornerRadius = 5
+        passwordTextfield.layer.cornerRadius = 5
+        loginButton.layer.cornerRadius = 4
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func loginActionButton() {
+    }
+    
+    
+    @IBAction func forgotPasswordActionButton() {
+    }
+    
+    
+
+}
